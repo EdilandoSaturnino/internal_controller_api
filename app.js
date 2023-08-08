9dd20402bd7cfbc1af94bdc703219c8714596317
Edilando Saturnino <35576130+EdilandoSaturnino@users.noreply.github.com>
2023-08-08 12:49:23 -0300

    feat: Health check route

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ app.use(express.json());
 app.use('/produtos', produtoRoutes);
 app.use('/clientes', clienteRoutes);
 
+app.get('/', (req, res) => {
+    const healthInfo = {
+        status: 'Up',
+        uptime: process.uptime(),
+        message: 'API está saudável',
+        timestamp: Date.now(),
+    };
+    res.status(200).json(healthInfo);
+});
+
+
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
