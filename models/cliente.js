97697614cf8b411ee2289298ed2a56a96956650a
Edilando Saturnino <35576130+EdilandoSaturnino@users.noreply.github.com>
2023-08-08 13:23:53 -0300

    feat: add news properties

--- models/cliente.js
+++ models/cliente.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const clienteSchema = new mongoose.Schema({
     internalCode: { type: String, required: true },
+    name: { type: String, required: false },
 });
 
 module.exports = mongoose.model('Cliente', clienteSchema);
--- models/produto.js
+++ models/produto.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const produtoSchema = new mongoose.Schema({
     internalCode: { type: String, required: true },
+    name: { type: String, required: false },
+    quantity: { type: Number, required: false },
+    price: { type: Number, required: false },
 });
 
 module.exports = mongoose.model('Produto', produtoSchema);
